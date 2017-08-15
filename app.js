ef7373d8770413e281fe12fbdcf03f97692c59c5
Sam Fereday <sam.fereday@shoutdigital.com>
2017-08-15 17:13:20 +0100

    Initial

---
+++ app.js
@@ -0,0 +1,233 @@
+function shuffle(array) {
+	var currentIndex = array.length, temporaryValue, randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+}
+
+///
+class Symbol {
+
+	constructor(n) {
+		this.n = n;
+		this.el = document.createElement('div');
+		this.el.classList.add('cell');
+		this.el.innerHTML = n;
+	}
+
+	hide() {
+		this.el.classList.remove('active');
+	}
+
+	show() {
+		this.el.classList.add('active');
+	}
+
+}
+
+///
+/*
+the process:
+Symbol are better than numbers for processing, more fun too if it's a game. Could even use arrows and / or buttons from keyboard.
+*/
+let cont = document.getElementById('container');
+let fixedMembers = [];
+
+for (let i = 0; i < 5; i++) {
+	fixedMembers.push(new Symbol(i));
+	cont.appendChild(fixedMembers[i].el);
+}
+
+/// Generate the sequence
+let seq = fixedMembers.map(x => x.n);
+
+// Make it happen
+let inc = 0;
+let sequenceRunning = false;
+
+function playSequence() {
+
+	if (inc > fixedMembers.length - 1) {
+		sequenceRunning = false;
+		inc = 0;
+		return;
+	} else {
+		sequenceRunning = true;
+	}
+
+	fixedMembers[seq[inc]].show();
+
+	setTimeout(function () {
+
+		fixedMembers[seq[inc]].hide();
+		inc += 1;
+
+		playSequence();
+
+	}, 250);
+
+}
+
+function checkSequence() {
+
+	// ...
+
+}
+
+//...
+document.getElementById('start').addEventListener('click', function () {
+
+	if (sequenceRunning)
+		return;
+
+	// Shuffle it
+	seq = shuffle(seq);
+
+	// Do it
+	playSequence();
+
+});
+
+// https://github.com/lostdecade/simple_canvas_game
+// Create the canvas
+var canvas = document.createElement("canvas");
+var ctx = canvas.getContext("2d");
+canvas.width = 512;
+canvas.height = 480;
+document.body.appendChild(canvas);
+
+// Background image
+var bgReady = false;
+var bgImage = new Image();
+bgImage.onload = function () {
+	bgReady = true;
+};
+bgImage.src = "images/background.png";
+
+// Hero image
+var heroReady = false;
+var heroImage = new Image();
+heroImage.onload = function () {
+	heroReady = true;
+};
+heroImage.src = "images/hero.png";
+
+// Monster image
+var monsterReady = false;
+var monsterImage = new Image();
+monsterImage.onload = function () {
+	monsterReady = true;
+};
+monsterImage.src = "images/monster.png";
+
+// Game objects
+var hero = {
+	speed: 256 // movement in pixels per second
+};
+var monster = {};
+var monstersCaught = 0;
+
+// Handle keyboard controls
+var keysDown = {};
+
+addEventListener("keydown", function (e) {
+	keysDown[e.keyCode] = true;
+}, false);
+
+addEventListener("keyup", function (e) {
+	delete keysDown[e.keyCode];
+}, false);
+
+// Reset the game when the player catches a monster
+var reset = function () {
+	hero.x = canvas.width / 2;
+	hero.y = canvas.height / 2;
+
+	// Throw the monster somewhere on the screen randomly
+	monster.x = 32 + (Math.random() * (canvas.width - 64));
+	monster.y = 32 + (Math.random() * (canvas.height - 64));
+};
+
+// Update game objects
+var update = function (modifier) {
+	if (38 in keysDown) { // Player holding up
+		hero.y -= hero.speed * modifier;
+	}
+	if (40 in keysDown) { // Player holding down
+		hero.y += hero.speed * modifier;
+	}
+	if (37 in keysDown) { // Player holding left
+		hero.x -= hero.speed * modifier;
+	}
+	if (39 in keysDown) { // Player holding right
+		hero.x += hero.speed * modifier;
+	}
+
+	// Are they touching?
+	if (
+		hero.x <= (monster.x + 32)
+		&& monster.x <= (hero.x + 32)
+		&& hero.y <= (monster.y + 32)
+		&& monster.y <= (hero.y + 32)
+	) {
+		++monstersCaught;
+		reset();
+	}
+};
+
+// Draw everything
+var render = function () {
+	if (bgReady) {
+		ctx.drawImage(bgImage, 0, 0);
+	}
+
+	if (heroReady) {
+		ctx.drawImage(heroImage, hero.x, hero.y);
+	}
+
+	if (monsterReady) {
+		ctx.drawImage(monsterImage, monster.x, monster.y);
+	}
+
+	// Score
+	ctx.fillStyle = "rgb(250, 250, 250)";
+	ctx.font = "24px Helvetica";
+	ctx.textAlign = "left";
+	ctx.textBaseline = "top";
+	ctx.fillText("Goblins caught: " + monstersCaught, 32, 32);
+};
+
+// The main game loop
+var main = function () {
+	var now = Date.now();
+	var delta = now - then;
+
+	update(delta / 1000);
+	render();
+
+	then = now;
+
+	// Request to do this again ASAP
+	requestAnimationFrame(main);
+};
+
+// Cross-browser support for requestAnimationFrame
+var w = window;
+requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
+
+// Let's play this game!
+var then = Date.now();
+reset();
+main();
